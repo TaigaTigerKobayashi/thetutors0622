c26e919555687256e7a6591d5a4ca77adc7dd0d9
TaigaTigerKobayashi <62683204+TaigaTigerKobayashi@users.noreply.github.com>
2020-06-27 22:51:02 +0900

    fb追加

--- tutors/user_insert.php
+++ tutors/user_insert.php
@@ -9,6 +9,7 @@ include "funcs.php";
 
 //1. POSTデータ取得
 $email      = filter_input( INPUT_POST, "email" );
+$fb      = filter_input( INPUT_POST, "fb" );
 $lid       = filter_input( INPUT_POST, "lid" );
 $lpw       = filter_input( INPUT_POST, "lpw" );
 // $lpw       = password_hash($lpw, PASSWORD_DEFAULT);   //パスワードハッシュ化
@@ -17,9 +18,10 @@ $lpw       = filter_input( INPUT_POST, "lpw" );
 $pdo = db_conn();
 
 //３．データ登録SQL作成
-$sql = "INSERT INTO tutors_user_table(email,lid,lpw,user_type,life_flg)VALUES(:email,:lid,:lpw,1,0)";
+$sql = "INSERT INTO tutors_user_table(email,fb,lid,lpw,user_type,life_flg)VALUES(:email,:fb,:lid,:lpw,1,0)";
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':email', $email, PDO::PARAM_STR); //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':fb', $fb, PDO::PARAM_STR); //Integer（数値の場合 PDO::PARAM_INT)
 $stmt->bindValue(':lid', $lid, PDO::PARAM_STR); //Integer（数値の場合 PDO::PARAM_INT)
 $stmt->bindValue(':lpw', $lpw, PDO::PARAM_STR); //Integer（数値の場合 PDO::PARAM_INT)::PARAM_STR);
  //Integer（数値の場合 PDO::PARAM_INT)
