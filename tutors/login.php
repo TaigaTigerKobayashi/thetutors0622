bc9560aa61050d9d5d0f0ba3abcfb00515a9669b
TaigaTigerKobayashi <62683204+TaigaTigerKobayashi@users.noreply.github.com>
2020-06-29 15:26:15 +0900

    パスワード忘れた方＋ログイン状態維持を非表示、

--- tutors/login.php
+++ tutors/login.php
@@ -41,14 +41,14 @@
     <input type="text" name="lid" id="inputUserID" class="form-control mb-3" placeholder="ユーザーID" required autofocus>
     <label for="inputPassword" class="sr-only">パスワード</label>
     <input type="password" name="lpw" id="inputPassword" class="form-control" placeholder="パスワード" required>
-    <div class="checkbox mb-3">
+    <!-- <div class="checkbox mb-3">
       <label>
         <input type="checkbox" value="remember-me"> ログイン状態を保持する
       </label>
-    </div>
+    </div> -->
     <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
     <div class="link-area">
-      <a class="password-forget" href="" style="display:block;">パスワードをお忘れですか？</a>
+      <!-- <a class="password-forget" href="" style="display:block;">パスワードをお忘れですか？</a> -->
       <a class="password-forget" href="contact.php" style="display:block;">お問い合わせはこちら</a>
       <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
         会員登録
